f367ef82c89e107ca429bd8915bc88bf82354b10
rsquare22-coder <r.rathi0726@gmail.com>
2024-08-28 12:37:17 -0400

    Create CartPage.js

---
+++ CartPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function CartPage() {
+  return (
+    <div className="cart-page">
+      <h2>Your Shopping Cart</h2>
+      {/* Cart items will be listed here */}
+      <p>Your cart is empty.</p>
+    </div>
+  );
+}
+
+export default CartPage;
